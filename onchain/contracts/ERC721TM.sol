257b5fc0da5717c0c91ded9fc2d4de35afcd76a4
Tointer <35668369+Tointer@users.noreply.github.com>
2023-11-20 00:23:05 +0300

    add erc721 contract

---
+++ onchain/contracts/ERC721TM.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract ERC721TM is ERC721, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    string public baseURI;
+
+    constructor(address owner, address minter, string symbol, string name, string _baseURI) ERC721(name, symbol) {
+        _grantRole(DEFAULT_ADMIN_ROLE, owner);
+        _grantRole(MINTER_ROLE, minter);
+        baseURI = _baseURI;
+    }
+
+    function mint(address to, uint256 tokenId) public onlyRole(MINTER_ROLE) {
+        _safeMint(to, tokenId);
+    }
+
+    function batchMint(address to, uint256[] memory tokenIds) public onlyRole(MINTER_ROLE) {
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            _safeMint(to, tokenIds[i]);
+        }
+    }
+
+    function changeBaseURI(string memory _baseURI) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        baseURI = _baseURI;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return "";
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
